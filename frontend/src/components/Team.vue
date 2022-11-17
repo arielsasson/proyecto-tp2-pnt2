85f78183a4f789ea34659b6eca54ceb71a6e44f4
Alan <alank_93@hotmail.com>
2022-11-16 22:45:30 -0300

    ConElTontoDeSasi

---
+++ frontend/src/components/Team.vue
@@ -0,0 +1,48 @@
+<script>
+
+export default {
+  name: "",
+  props: ['team'],
+  data() {
+    return {
+      arriba: false,
+      position: null
+    };
+  },
+
+};
+
+
+</script>
+
+<template> 
+<div>
+    <p @mouseenter="arriba = true" @mouseleave="arriba = false" class="div" :class="{'div-hover': arriba}"> {{team.Name}}</p>
+</div>
+
+</template>
+
+
+<style>
+.div {
+  background-color: white;
+  border-style: solid;
+  
+  
+}
+
+.div-hover {
+  background-color: lightskyblue;
+  opacity: 1;
+  
+  
+}
+
+p:hover{
+  font-size: 115%;
+  text-align: center;
+  font-weight: 900;
+  color: black;
+}
+
+</style>
