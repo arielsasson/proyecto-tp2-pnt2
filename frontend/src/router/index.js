768b61ca7e3c24b279e6129b22e19078a38512f9
arielsasson <69323248+arielsasson@users.noreply.github.com>
2022-11-16 23:54:58 -0300

    sddsad

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../views/HelloWorld.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import GroupsView from '../views/GroupsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
