6eae7172ef8ca8b364bcc27b3e6bec228556ca82
arielsasson <69323248+arielsasson@users.noreply.github.com>
2022-11-24 17:50:25 -0300

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,22 @@ app.use(bodyParser.json());
 new GroupRouter(app, "/api/groups");
 new UsersRouter(app, "/api/users");
 new PlayoffRouter(app, "/api/playoffs");
-app.use(cors());
+// Allowed origins
+const allowedOrigins = ["http://localhost:5173", "https://127.0.0.1:5173"];
+// Do you want to skip the checking of the origin and grant authorization?
+const skipTheCheckingOfOrigin = false;
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || skipTheCheckingOfOrigin)
+            return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = "The CORS policy for this site does not " +
+                "allow access from the specified Origin.";
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 app.get('/', (req, res) => {
     res.send('asdasd');
 });
